3fe2b127034e68667374b4b75d97c4b9d88a195f
izzee <ichabon@me.com>
2018-04-24 15:45:09 -0400

    fixed

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -6,26 +6,19 @@ export default class Topbar extends React.Component {
   render(){
     return(
       <div className="topbar">
-        {/* <div className="topbar-row-1"> */}
           <div className="left-buttons">
             <div className="close-button"></div>
             <div className="min-button"></div>
             <div className="max-button"></div>
           </div>
           <div className="topbar-title">{this.props.activeContent}</div>
-        {/* </div> */}
-        {/* <div className="topbar-row-2"> */}
           <div className="forward-back">
             <div className="back-button"><ChevronLeft/></div>
             <div className="forward-button"><ChevronRight/></div>
           </div>
-          <div className="display-modes">
-
-          </div>
           <form>
             <input className="search-field" placeholder="Search"></input>
           </form>
-        {/* </div> */}
       </div>
     )
   }
